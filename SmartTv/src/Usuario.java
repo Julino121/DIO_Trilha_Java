30cb27b2b1d8d1a3356361d4e802ce0145cabc97
Julio <julio.cesar.reali@gmail.com>
2024-05-13 12:41:23 -0300

    Primeiros Testes

---
+++ SmartTv/src/Usuario.java
@@ -0,0 +1,24 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception{
+
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+
+        System.out.println("TV ligada?: " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " + smartTv.volume);
+
+        smartTv.escolherCanal(13);
+        System.out.println("Canal Atual: " + smartTv.canal);
+
+        smartTv.ligar();
+        System.out.println("TV ligada?: " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("TV ligada?: " + smartTv.ligada);
+
+    }
+}
